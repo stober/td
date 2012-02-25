68713a5d73a0de3e13581f2b8cb1ab3c5f0b3c48
Jeremy Stober <stober@gmail.com>
2012-02-24 18:05:26 -0600

    Debugging different TD implementations.

--- bin/td_example.py
+++ bin/td_example.py
@@ -8,10 +8,20 @@ Description: Examples using TD algorithms to learn value functions.
 
 
 from gridworld.boyan import Boyan
-from td import TD
+from gridworld.chainwalk import Chainwalk
+from td import TD, TDQ
 
 # a simple environment
 env = Boyan()
 learner = TD(13, 0.1, 1.0, 0.8)
 learner.learn(1000,env,env.random_policy)
 print learner.V
+
+env = Chainwalk()
+learnerq = TDQ(2,4, 0.1, 0.9, 0.8)
+
+import pdb
+pdb.set_trace()
+learnerq.learn(1000,env,env.random_policy,episodic = False)
+print learnerq.V
+
--- src/__init__.py
+++ src/__init__.py
@@ -21,7 +21,8 @@ class TDCmac(object):
     def value(self, action, vector):
         return self.cmac[action].eval(vector)
 
-    def train(self, action, pvector, reward, vector):
+    def train(self, action, pvector, reward, vector, next_action):
+        # This has an error. Needs to include the next action.
         self.cmac[action].train(vector, pvector, reward)
 
     def reset(self):
@@ -33,36 +34,6 @@ class TDCmac(object):
         for cmac in self.cmac:
             values.append(cmac.eval(vector))
         return np.argmax(values)
-
-class TDLinear(object):
-    """
-    The same training algorithm as lstd.td but packaged differently
-    with a slightly more flexible interface (can call train on single
-    steps for instance).
-    """
-
-    def __init__(self, nfeatures, alpha, gamma, ld):
-        self.params = np.zeros(nfeatures)
-        self.e = np.zeros(nfeatures)
-        self.nfeatures = nfeatures
-        self.alpha = alpha # learning rate
-        self.gamma = gamma # discount
-        self.ld = ld # lambda
-
-    def value(self,features):
-        return np.dot(self.params,features)
-
-    def train(self, pfeatures, reward, features):
-        # basically does one step of training
-        # note that the gradient of the value function wrt the params are the current features
-
-        delta = reward + (self.gamma * self.value(features)) - self.value(pfeatures)
-
-        self.e = self.gamma * self.ld * self.e + pfeatures
-        self.params = self.params + self.alpha * delta * self.e
-
-    def reset(self):
-        self.e = np.zeros(self.nstates)
 
 class TD(object):
     """
@@ -95,22 +66,97 @@ class TD(object):
 
         delta = self.delta(pstate, reward, state)
 
-        self.e[pstate] = 1 # replacing traces
+        self.e[pstate] = 1.0 # replacing traces
 
-        for s in range(self.nstates):
-            self.V[s] += self.alpha * delta * self.e[s]
-            self.e[s] *= (self.gamma * self.ld)
+        #for s in range(self.nstates):
+        self.V += self.alpha * delta * self.e
+        self.e *= (self.gamma * self.ld)
 
     def learn(self, nepisodes, env, policy):
         # learn for niters episodes with resets
         for i in range(nepisodes):
             self.reset()
             t = env.single_episode(policy) # includes env reset
-            for (previous, action, reward, next) in t:
-                self.train(previous, reward, next)
+            for (previous, action, reward, state, next_action) in t:
+                self.train(previous, reward, state)
 
     def reset(self):
         self.e = np.zeros(self.nstates)
 
-class Sarsa(TD):
-    pass
+class TDQ(object):
+    """
+    Discrete action-value function approximation via temporal difference learning.
+    """
+    def __init__(self, nactions, nstates, alpha, gamma, ld):
+        self.V = np.zeros((nactions, nstates))
+        self.e = np.zeros((nactions, nstates))
+        self.nactions = nactions
+        self.nstates = nstates
+        self.alpha = alpha # learning rate
+        self.gamma = gamma # discount
+        self.ld = ld # lambda
+
+    def value(self, action, state):
+        return self.V[action, state]
+
+    def delta(self, pstate, paction, reward, state, action):
+        """
+        This is the core error calculation. Note that if the value
+        function is perfectly accurate then this returns zero since by
+        definition value(pstate) = gamma * value(state) + reward.
+        """
+        return reward + (self.gamma * self.value(action,state)) - self.value(paction,pstate)
+
+    def train(self, pstate, paction, reward, state, action):
+        """
+        A single step of reinforcement learning.
+        """
+
+        delta = self.delta(pstate, paction, reward, state, action)
+
+        self.e[paction,pstate] = 1.0 # replacing traces
+
+        self.V += self.alpha * delta * self.e
+        self.e *= (self.gamma * self.ld)
+
+    def learn(self, nepisodes, env, policy, episodic = True):
+        # learn for nepisodes with resets
+        if episodic:
+            for i in range(nepisodes):
+                self.reset()
+                t = env.single_episode(policy) # includes env reset
+                for (previous, paction, reward, state, action) in t:
+                    self.train(previous, paction, reward, state, action)
+        else:
+            for i in range(nepisodes):
+                t = env.trace(nepisodes, policy)
+                for (previous, paction, reward, state, action) in t:
+                    self.train(previous, paction, reward, state, action)
+
+    def reset(self):
+        self.e = np.zeros((self.nactions, self.nstates))
+
+
+class TDLinear(TD):
+    """
+    A more general linear value function representation.
+    """
+
+    def __init__(self, nfeatures, alpha, gamma, ld):
+        self.params = np.zeros(nfeatures)
+        self.e = np.zeros(nfeatures)
+        self.nfeatures = nfeatures
+        self.alpha = alpha # learning rate
+        self.gamma = gamma # discount
+        self.ld = ld # lambda
+
+    def value(self,features):
+        return np.dot(self.params,features)
+
+    def train(self, pfeatures, reward, features):
+        delta = self.delta(pfeatures, reward, features)
+        self.e = self.gamma * self.ld * self.e + pfeatures
+        self.params = self.params + self.alpha * delta * self.e
+
+    def reset(self):
+        self.e = np.zeros(self.nstates)
